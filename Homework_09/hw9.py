f701984b769b26a4772ea5736f9e82524b9ec018
ArneKlages4444 <aklages@uni-osnabrueck.de>
2022-01-14 16:55:13 +0100

    fix

--- Homework_09/hw9.py
+++ Homework_09/hw9.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
 from tensorflow.keras.layers import Dense, Activation, Conv2D, MaxPool2D, Conv2DTranspose, Flatten, Reshape, \
     BatchNormalization, Rescaling
 import matplotlib.pyplot as plt
@@ -26,7 +25,7 @@ class CandleDiscriminator(tf.keras.Model):
     def __init__(self):
         super(CandleDiscriminator, self).__init__()
         self.scaling = Rescaling(1. / 255)
-        self.conv_layer1 = Conv2D(filters=64, kernel_size=3, padding='same', activation=tf.nn.relu)
+        self.conv_layer1 = Conv2D(filters=16, kernel_size=3, padding='same', activation=tf.nn.relu)
         self.pooling1 = MaxPool2D(pool_size=2, strides=2)
         self.b_norm1 = BatchNormalization()
         self.activation1 = Activation(activation=tf.nn.relu)
@@ -34,8 +33,8 @@ class CandleDiscriminator(tf.keras.Model):
         self.pooling2 = MaxPool2D(pool_size=2, strides=2)
         self.b_norm2 = BatchNormalization()
         self.activation2 = Activation(activation=tf.nn.relu)
-        self.conv_layer3 = Conv2D(filters=16, kernel_size=3, padding='same', activation=tf.nn.relu)
-        self.flatten = Flatten()
+        self.conv_layer3 = Conv2D(filters=64, kernel_size=3, padding='same', activation=tf.nn.relu)
+        self.flatten = Flatten()  # or global pooling
         self.out = Dense(1, activation=tf.nn.sigmoid)
 
     @tf.function
@@ -90,7 +89,7 @@ def train(generator, discriminator, dataset, num_epochs=10, batch_size=32,
           generator_optimizer=tf.keras.optimizers.Adam(0.01)):
     for epoch in range(num_epochs):
         for data in dataset:
-            with tf.GradientTape() as gradient_tape, tf.GradientTape() as D_tape:
+            with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
                 fake_data = generator(tf.random.normal([batch_size, generator.input_size]))
                 fake_data_pred = discriminator(fake_data)
                 real_data_pred = discriminator(data)
@@ -98,27 +97,24 @@ def train(generator, discriminator, dataset, num_epochs=10, batch_size=32,
                 generator_loss = -tf.math.reduce_mean(tf.math.log(real_data_pred) + tf.math.log(1 - fake_data_pred))
                 discriminator_loss = tf.math.reduce_mean(tf.math.log(1 - fake_data_pred))
 
-                discriminator_gradients = D_tape.gradient(discriminator_loss, discriminator.trainable_variables)
+                discriminator_gradients = discriminator_tape.gradient(discriminator_loss,
+                                                                      discriminator.trainable_variables)
                 discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
 
-                generator_gradients = gradient_tape.gradient(generator_loss, generator.trainable_variables)
+                generator_gradients = generator_tape.gradient(generator_loss, generator.trainable_variables)
                 generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
 
 
-categories = [line.rstrip(b'\n') for line in urllib.request.urlopen(
-    'https://raw.githubusercontent.com/googlecreativelab/quickdraw-dataset/master/categories.txt')]
-print(categories[:10])
 category = 'candle'
 if not os.path.isdir('npy_files'):
     os.mkdir('npy_files')
 url = f'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/{category}.npy'
 urllib.request.urlretrieve(url, f'npy_files/{category}.npy')
-
 images = np.load(f'npy_files/{category}.npy')
 
 train_ds, test_ds = np.split(images, [int(.7 * len(images))])
-train_ds = tf.data.Dataset.from_tensor_slices(train_ds.values)
-test_ds = tf.data.Dataset.from_tensor_slices(test_ds.values)
+train_ds = tf.data.Dataset.from_tensor_slices(train_ds)
+test_ds = tf.data.Dataset.from_tensor_slices(test_ds)
 
 train_dataset = train_ds.apply(prepare_data)
 test_dataset = test_ds.apply(prepare_data)
@@ -131,6 +127,6 @@ discriminator = CandleDiscriminator()
 train(generator, discriminator, train_dataset)
 
 # TODO: visualize examples
-for d in CandleGenerator(test_dataset.take(1)):
+for d in generator(test_dataset.take(1)):
     plt.imshow(d[0].numpy().astype("uint8")[:, :, 0], cmap='gray')
     plt.show()
