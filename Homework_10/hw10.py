3c9f5d2d1e13aa32937a641cef755175d3f45141
ArneKlages4444 <aklages@uni-osnabrueck.de>
2022-01-26 16:52:56 +0100

    fix

--- Homework_10/hw10.py
+++ Homework_10/hw10.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Layer, Input
-from collections import Counter
+from tensorflow.keras.layers import Dense, Input
 import tensorflow as tf
 import tensorflow_text as tf_txt
+from collections import Counter
 from pathlib import Path
 import re
 
@@ -16,10 +16,6 @@ def tokenize_text(text):
     return sp.split(text).numpy()
 
 
-def build_index_to_token(vocab):
-    return {i: t for t, i in zip(vocab, range(len(vocab)))}
-
-
 def build_token_to_index(vocab):
     return {t: i for t, i in zip(vocab, range(len(vocab)))}
 
@@ -28,7 +24,6 @@ def preprocess_text(text, bow_size=10000, window_size=4):
     text = handle_special_and_lower(text)
     tokens = tokenize_text(text)
     common_tokens = dict(Counter(tokens).most_common(bow_size)).keys()
-    i2t = build_index_to_token(common_tokens)
     t2i = build_token_to_index(common_tokens)
     value = []
     target = []
@@ -45,7 +40,7 @@ def preprocess_text(text, bow_size=10000, window_size=4):
                     if ta in t2i:
                         value.append(t2i[to])
                         target.append(t2i[ta])
-    return (value, target), i2t, t2i
+    return (value, target), t2i
 
 
 def prepare_data(ds):
@@ -57,30 +52,33 @@ class WordEmbedding(Dense):
     def __init__(self, embedding_size):
         super(WordEmbedding, self).__init__(embedding_size, use_bias=False)
 
-    def word_to_vec(self, one_hot_word):
-        pass  # TODO
+    def word_to_vec(self, one_hot_word_index):
+        return self.weights[0][one_hot_word_index]
 
 
 def scip_gram_model(embedding_size, bow_size):
     inp = Input(shape=(bow_size))
-    x = WordEmbedding(embedding_size)(inp)
+    emb = WordEmbedding(embedding_size)
+    x = emb(inp)
     out = Dense(bow_size, activation=tf.nn.softmax)(x)
-    return tf.keras.Model(inp, out)
+    return tf.keras.Model(inp, out), emb
 
 
 bow_size = 10000
+embedding_size = 128
 
 data = Path('bible.txt').read_text()
-(data, i2t, t2i) = preprocess_text(data, bow_size)
+(data, t2i) = preprocess_text(data, bow_size)
 
 train_dataset = tf.data.Dataset.from_tensor_slices(data).map(
-    lambda v, t: (tf.one_hot(v, depth=len(t2i)), tf.one_hot(t, depth=len(t2i)))).apply(prepare_data)
-
-print(train_dataset.take(1))
+    lambda v, t: (tf.one_hot(v, depth=bow_size), tf.one_hot(t, depth=bow_size))).apply(prepare_data)
 
 tf.keras.backend.clear_session()
 
-model = scip_gram_model(100, bow_size)
-model.summary()
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model, emb = scip_gram_model(embedding_size, bow_size)
+
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.CategoricalCrossentropy(),
+              metrics=['accuracy'])
 model.fit(train_dataset, epochs=10)
+
+emb.word_to_vec(t2i[b'king'])
