57d767275fbac218ecbbd9c7aac1291d8ec6165b
Erik Nickel <ernickel@uos.de>
2022-03-28 12:50:14 +0200

    fix

--- Final_Project_Test/data.py
+++ Final_Project_Test/data.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
 
 import pandas as pd
+
 class DatasetPreprossesing():
 
 
@@ -26,15 +26,18 @@ class DatasetPreprossesing():
 
         omni_raw = omni_raw.drop(['minutes','n_steps','n_ingredients', 'name', 'contributor_id', 'submitted', 'tags', 'nutrition', 'steps', 'description', 'review', 'ingredients'], axis=1)
         omni_raw = pd.merge(omni_raw,recipies_pp.drop(['i','name_tokens','steps_tokens','techniques','calorie_level', 'ingredient_tokens',],axis = 1),on='recipe_id')
+        
+        counts = omni_raw.value_counts('user_id')
         omni_raw = omni_raw[omni_raw.rating >= 4].drop(['rating'], axis = 1)
         omni_raw = omni_raw.loc[omni_raw['user_id'].isin(counts.index[counts >= 6])]
 
         omni_raw = omni_raw.sort_values(['user_id','date']).reset_index()
 
         omni_raw.to_csv('data_pp.csv')
+        
         self.num_inter = omni_raw.value_counts('user_id').to_numpy()[1]
         print(self.num_inter)
     
 
 new = DatasetPreprossesing()
-new.preprocessing('RAW_interactions.csv','RAW_recipes.csv','PP_recipes.csv')
+new.preprocessing()
