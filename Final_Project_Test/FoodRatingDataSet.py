a9cb21dadbfc3d06dd13bfd8770726675bab4705
Erik Nickel <ernickel@uos.de>
2022-03-28 11:57:16 +0200

    fix

--- Final_Project_Test/FoodRatingDataSet.py
+++ Final_Project_Test/FoodRatingDataSet.py
@@ -1,6 +1,16 @@
+from tensorflow.keras.layers import CategoryEncoding
+
+
 class FoodRatingDataset:
 
     def __init__(self):
-        pass
+        super(FoodRatingDataset, self).__init__()
+        self.catenc = CategoryEncoding(num_tokens=8023, output_mode="multi_hot")
+
+    def __call__(self,id,ing,ofe):
+        return id,self.catenc(ing),ofe
+
+
+
 
 # tf.reduce_max(tf.one_hot(labels, num_classes, dtype=tf.int32), axis=0)
