9ac562df5038509a018d4e4b45efbcdeb46e3dca
AKS <aklages@uni-osnabrueck.de>
2022-04-02 16:41:06 +0200

    fix

--- Final_Project_Test/training.py
+++ Final_Project_Test/training.py
@@ -1,40 +1,67 @@
 from FoodRecommenderModelSequence import FoodRecommenderModelSequence
-import tensorflow as tf
-import keras
-import pandas as pd
-from DatasetPreprossesing import DatasetPreprossesing
 from FoodRatingDataSet import FoodRatingDataset
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint
+from datetime import datetime
+import tensorflow as tf
+import os.path
 
-#from TestDataSet import TestDataSet
-
-
+NUM_RECIPES = 178265  # 163690 #161880
 NUM_ING = 8023
 OTHER_FEATURES = 3
-
-NUM_RECIPES = 178265 #163690 #161880
-
-
-BATCHSIZE = 1
-
 SEQ_LEN = 9
+BATCH_SIZE = 64
 
-#ds = TestDataSet()
-#rec = FoodRecommenderModelSequence(recipe_count=NUM_RECIPES, seq_len=SEQ_LEN)
-#for (d, v) in ds.data():
+
+# ds = TestDataSet()
+# rec = FoodRecommenderModelSequence(recipe_count=NUM_RECIPES, seq_len=SEQ_LEN)
+# for (d, v) in ds.data():
 #    x = rec(d)
 #    print(x)
 
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~newnew~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-newnew = FoodRatingDataset()
-#print(newnew.data())
-data = newnew.data()
+
+def model_summary():
+    mod = create_model(SEQ_LEN)
+    ids, ing, ofe = tf.keras.Input(shape=[SEQ_LEN]), tf.keras.Input(shape=[SEQ_LEN, NUM_ING]), tf.keras.Input(
+        shape=[SEQ_LEN, OTHER_FEATURES])
+    mod((ids, ing, ofe))
+    mod.summary()
 
 
-print("DATA: ", data)
-#ids, ing, ofe = tf.keras.Input(shape=[20]), tf.keras.Input(shape=[20, 6000]), tf.keras.Input(shape=[20, 3])
+def create_model(seq_len):
+    return FoodRecommenderModelSequence(recipe_count=NUM_RECIPES, seq_len=seq_len)
 
-rec = FoodRecommenderModelSequence(recipe_count=NUM_RECIPES, seq_len=SEQ_LEN)
-rec.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001), metrics=['accuracy'])
-#x = rec(data)
-rec.fit(data, epochs=10)
-#print(x)
+
+def train(data, model):
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
+
+    board_file_name = datetime.now().strftime("%d%m%Y_%H%M%S")
+    checkpoints_dir = "checkpoints/checkpoint"
+
+    if os.path.exists(checkpoints_dir):
+        print("load model:")
+        board_file_name = f"{board_file_name}_loaded"
+        model.load_weights(checkpoints_dir)
+    else:
+        print("new model:")
+
+    tensorboard = TensorBoard(log_dir=f"logs/recommend_{board_file_name}")
+    checkpoints = ModelCheckpoint(filepath=checkpoints_dir, save_weights_only=True)
+
+    # model.fit(data, epochs=10, validation_split=0.3, callbacks=[tensorboard, checkpoints])
+    model.fit(data, epochs=10, callbacks=[tensorboard, checkpoints])
+
+
+def run():
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TRAINING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    dataset = FoodRatingDataset()
+    data = dataset.data()
+    print("DATA: ", data)
+    model_summary()
+    model = create_model(SEQ_LEN)
+    train(data, model)
+
+
+if __name__ == "__main__":
+    run()
