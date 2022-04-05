40f830718bfda2d5fe8599f5e65b171414599f7d
Erik Nickel <ernickel@uos.de>
2022-04-05 15:49:17 +0200

    train and val

--- Final_Project_Test/DatasetPreprossesing.py
+++ Final_Project_Test/DatasetPreprossesing.py
@@ -9,9 +9,9 @@ from tensorflow.keras.layers import CategoryEncoding
 class DatasetPreprossesing():
 
     def __init__(self,seq_len) -> None:
-        self.__seq_len = seq_len
+        self.seq_len = seq_len
 
-    def preprocessing(self, dataset1='RAW_interactions.csv', dataset2='RAW_recipes.csv', dataset3='PP_recipes.csv'):
+    def preprocessing(self, dataset1='RAW_interactions.csv', dataset2='RAW_recipes.csv', dataset3='PP_recipes.csv', train_size = 0.70):
         inter_raw = pd.read_csv(dataset1)
         recipes_raw = pd.read_csv(dataset2)
         recipies_pp = pd.read_csv(dataset3)
@@ -40,33 +40,22 @@ class DatasetPreprossesing():
         omni_raw = omni_raw.sort_values(['user_id', 'date']).reset_index()
         omni_raw.pop('date')
 
-        omni_raw.to_csv('data_pp.csv')
+        data_train, data_val = np.split(omni_raw.sample(frac=1, random_state=42), [int(train_size * len(omni_raw))])
+        data_train.to_csv('data_train.csv')
+        data_val.to_csv('data_val.csv')
 
         self.num_inter = omni_raw['user_id'].value_counts(sort = False).to_numpy()
         
         #self.num_inter = self.num_inter[:100]
-        print(len(self.num_inter))
         
         self.catEnc = CategoryEncoding(num_tokens=8023, output_mode="multi_hot")
-        #self.data = omni_raw
-        #self.data = self.data.drop(['index','user_id'], axis = 1)
-        #print(0)
-        #self.data = pd.read_csv('data_pp.csv', quotechar= '"', sep=',', converters={'recipe_features':ast.literal_eval,'ingredient_ids':ast.literal_eval}, usecols= ['recipe_id','recipe_features','ingredient_ids']).reset_index()
-        #print(1)
-        #self.data['ingredient_ids'] = self.data['ingredient_ids'].apply(lambda x: self.catEnc(x))
-        #print(2)
-        #self.data['recipe_features'] = self.data['recipe_features'].apply(lambda x: np.array(x))
-        #print(3)
-        #print(self.num_inter)
-        #print(omni_raw['user_id'].value_counts(sort = False))
-
-
+       
         del omni_raw
         gc.collect()
 
-    def genData(self, seq_len = self.seq_len, stepSize=1,):
+    def gen_data_train(self):
         skipRows = 0
-        readRows = seq_len
+        readRows = self.seq_len
         n = 0
 
         while n < len(self.num_inter): 
@@ -74,7 +63,7 @@ class DatasetPreprossesing():
 
             m = 1
             while m < self.num_inter[n] - readRows:
-                data = pd.read_csv('data_pp.csv', skiprows=skipRows + m, nrows=readRows, header=None, quotechar='"',
+                data = pd.read_csv('data_train.csv', skiprows=skipRows + m, nrows=readRows, header=None, quotechar='"',
                                    sep=',',
                                    converters={'recipe_features': ast.literal_eval, 'ing_ids': ast.literal_eval},
                                    names=['recipe_id', 'user_id', 'recipe_features', 'ing_ids']).reset_index()
@@ -91,31 +80,31 @@ class DatasetPreprossesing():
             skipRows += self.num_inter[n]
             n += 1
 
-    def genData2(self, windowsSize=10, stepSize=1):
-
-        from_index = 0
-
-        skipRows = stepSize
-        readRows = windowsSize
+    def gen_data_val(self):
+        skipRows = 0
+        readRows = self.seq_len
         n = 0
-        while True:
-            m = 0
-            to_index = self.num_inter[n] + 1
-            data_user = self.data.iloc[from_index:to_index]
 
-            while m < self.num_inter[n]+1 - readRows:
-                
-                data_user_window = data_user.iloc[m:m+readRows]
-                
-                data_user_window.ingredient_ids = data_user_window.ingredient_ids.apply(lambda x: self.catEnc(x))
-                
+        while n < len(self.num_inter): 
+
+
+            m = 1
+            while m < self.num_inter[n] - readRows:
+                data = pd.read_csv('data_val.csv', skiprows=skipRows + m, nrows=readRows, header=None, quotechar='"',
+                                   sep=',',
+                                   converters={'recipe_features': ast.literal_eval, 'ing_ids': ast.literal_eval},
+                                   names=['recipe_id', 'user_id', 'recipe_features', 'ing_ids']).reset_index()
+                data['ing_ids'] = data['ing_ids'].apply(
+                    lambda x: self.catEnc(x))
+                #data['recipe_features'] = data['recipe_features'].apply(lambda x: np.array(x))
+
                 m += 1
-                
-                yield (tf.convert_to_tensor(data_user_window.recipe_id.tolist()[:-1]), tf.convert_to_tensor(data_user_window.ingredient_ids.tolist()[:-1]), tf.convert_to_tensor(data_user_window.recipe_features.tolist()[:-1])),tf.convert_to_tensor(data_user_window.recipe_id.tolist()[-1])
-
-                del data_user_window
-                gc.collect()
-            from_index += to_index
+                # if m == 2:
+                #    print((data.recipe_id.tolist()[1], data.ing_ids.tolist()[1], data.recipe_features.tolist()[1]),data.recipe_id.tolist()[-1])
+                yield (tf.convert_to_tensor(data.recipe_id.tolist()[:-1]),
+                       tf.convert_to_tensor(data.ing_ids.tolist()[:-1]),
+                       tf.convert_to_tensor(data.recipe_features.tolist()[:-1])),tf.convert_to_tensor(data.recipe_id.tolist()[-1])
+            skipRows += self.num_inter[n]
             n += 1
-            del data_user
-            gc.collect()
+
+  
--- Final_Project_Test/FoodRatingDataSet.py
+++ Final_Project_Test/FoodRatingDataSet.py
@@ -19,7 +19,8 @@ class FoodRatingDataset:
         super(FoodRatingDataset, self).__init__()
         self.new = DatasetPreprossesing(seq_len + 1)
         self.new.preprocessing()
-        self.dataset = tf.data.Dataset.from_generator(self.new.genData,output_signature=((tf.TensorSpec(shape=(seq_len), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,8023), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,3), dtype=tf.int32)),tf.TensorSpec(shape=(), dtype=tf.int32)))
+        self.data_train = tf.data.Dataset.from_generator(self.new.gen_data_train,output_signature=((tf.TensorSpec(shape=(seq_len), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,8023), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,3), dtype=tf.int32)),tf.TensorSpec(shape=(), dtype=tf.int32)))
+        self.data_val = tf.data.Dataset.from_generator(self.new.gen_data_val,output_signature=((tf.TensorSpec(shape=(seq_len), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,8023), dtype=tf.int32),tf.TensorSpec(shape=(seq_len,3), dtype=tf.int32)),tf.TensorSpec(shape=(), dtype=tf.int32)))
 
 
 
